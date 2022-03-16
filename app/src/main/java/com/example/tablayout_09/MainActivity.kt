77b54f01ad77b4820343651678ef6436a69cff61
Hansen <tofvoid@gmail.com>
2022-03-16 11:59:34 +0700

    Initial commit(unfinished)

---
+++ app/src/main/java/com/example/tablayout_09/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tablayout_09
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        view_pager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(view_pager)
+        supportActionBar?.elevation = 0f
+    }
+}
